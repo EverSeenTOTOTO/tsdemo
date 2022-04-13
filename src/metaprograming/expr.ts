dc1fe59c0c664ca18034977e39268333ab4752d1
EverSeenTOTOTO <everseenflash@163.com>
2022-04-13 11:10:29 +0800

    refactor: optimize metaprogram

--- src/metaprograming/expr.ts
+++ src/metaprograming/expr.ts
@@ -3,6 +3,7 @@
 import {
   uint, add, ge, _0, _1, _2, is_same,
 } from './base';
+import { match } from './list';
 
 // 右侧并不重要，只是需要一个独一无二的类型
 
@@ -32,27 +33,46 @@ export type apply<Closure, Args> = Closure extends closure<lambda<infer Ph, infe
   ? evaluate<Body, binding<Ph, Args, Env>>
   : never;
 
-export type eval_add<Lhs, Rhs> = Lhs extends uint
-  ? Rhs extends uint
-    ? is_same<Lhs, never> extends false
-      ? is_same<Rhs, never> extends false
-        ? add<Lhs, Rhs>
+export type eval_lit<T> = T extends lit<infer Val>
+  ? Val
+  : never;
+
+export type eval_ph<T, Env> = T extends ph<infer Name>
+  ? env_lookup<ph<Name>, Env>
+  : never;
+
+export type eval_lambda<T, Env> = T extends lambda<infer Ph, infer Body>
+  ? closure<lambda<Ph, Body>, Env>
+  : never;
+
+export type eval_app<T, Env> = T extends app<infer Func, infer Args>
+  ? apply<evaluate<Func, Env>, evaluate<Args, Env>>
+  : never;
+
+export type eval_add<T, Env> = T extends add_expr<infer TL, infer TR>
+  ? evaluate<TL, Env> extends infer Lhs
+    ? evaluate<TR, Env> extends infer Rhs
+      ? Lhs extends uint
+        ? Rhs extends uint
+          ? is_same<Lhs, never> extends false
+            ? is_same<Rhs, never> extends false
+              ? add<Lhs, Rhs>
+              : never
+            : never
+          : never
         : never
       : never
     : never
   : never;
 
-export type evaluate<T, Env> = T extends lit<infer Val>
-  ? Val
-  : T extends ph<infer Name>
-    ? env_lookup<ph<Name>, Env>
-    : T extends lambda<infer Ph, infer Body>
-      ? closure<lambda<Ph, Body>, Env>
-      : T extends app<infer Func, infer Args>
-        ? apply<evaluate<Func, Env>, evaluate<Args, Env>>
-        : T extends add_expr<infer Lhs, infer Rhs>
-          ? eval_add<evaluate<Lhs, Env>, evaluate<Rhs, Env>>
-          : never;
+export type evaluate<T, Env> = match<T, [
+  [lit<any>, eval_lit<T>],
+  [ph<any>, eval_ph<T, Env>],
+  [lambda<any, any>, eval_lambda<T, Env>],
+  [app<any, any>, eval_app<T, Env>],
+  [add_expr<any, any>, eval_add<T, Env>],
+  [any, never],
+]>;
 
 // placeholder
 export type x = { __ph__: 'x' };
@@ -63,4 +83,4 @@ export type y = { __ph__: 'y' };
 type foo = evaluate<app<lambda<x, x>, lit<_2>>, empty_env>;
 // ((x) => (y) => x + y)(1)(2)
 // ((lambda (x) (lambda (y) (+ x y))) 1) 2)
-type bar = evaluate<app<app<lambda<x, lambda<y, add_expr<x, y>>>, lit<_2>>, lit<_2>>, empty_env>;
+type bar = evaluate<app<app<lambda<x, lambda<y, add_expr<x, y>>>, lit<_1>>, lit<_2>>, empty_env>;
