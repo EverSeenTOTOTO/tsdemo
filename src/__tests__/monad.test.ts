f8b42af621a1d0536619c81db3c4dbe383bc4696
EverSeenTOTOTO <everseenflash@163.com>
2024-06-22 01:39:47 +0800

    feat: pending

--- src/__tests__/monad.test.ts
+++ src/__tests__/monad.test.ts
@@ -1,10 +1,166 @@
-import { Box, map } from '../monad';
+import { List, Pending } from '../monad';
 
-const identity = <T>(x: T) => x;
+const a = new List([1, 2, 3]);
+const b = new List([1, 2, 3]);
+const c = new List([2, 3, 4]);
 
-it('test identity law', () => {
-  const x = Box.of(42);
-  const mapped = map(identity)(x);
-
-  expect(mapped.value).toBe(x.value);
+it('Setoid', () => {
+  expect(a.equals(b)).toBe(true);
+  expect(a.equals(b)).toBe(b.equals(a));
 });
+
+it('Semigroup', () => {
+  expect(
+    a.concat(b).concat(c).equals(a.concat(b.concat(c))),
+  ).toBe(true);
+});
+
+it('Monoid', () => {
+  expect(a.concat(List.empty()).equals(a)).toBe(true);
+  expect(List.empty().concat(a).equals(a)).toBe(true);
+});
+
+const id = <T>(value: T): T => value;
+const f = (value: number) => value + 1;
+const g = (value: number) => value * 2;
+
+it('Functor', () => {
+  expect(a.map(id).equals(a)).toBe(true);
+  expect(a.map((x) => f(g(x))).equals(a.map(g).map(f))).toBe(true);
+});
+
+const F = new List([f, g]);
+const G = new List([
+  (value: number) => value % 2 === 0,
+  (value: number) => value % 3 === 0,
+]);
+
+it('Apply', () => {
+  expect(a.ap(F).ap(G).equals(
+    a.ap(F.ap(G.map((gn) => (fn) => (n) => gn(fn(n))))),
+  )).toBe(true);
+});
+
+it('Applicative', () => {
+  expect(a.ap(List.of(id)).equals(a)).toBe(true);
+  expect(
+    List.of(42).ap(List.of(f)).equals(
+      List.of(f(42)),
+    ),
+  );
+  expect(
+    List.of(42).ap(F).equals(
+      F.ap(List.of((fn) => fn(42))),
+    ),
+  );
+});
+
+it('Foldable', () => {
+  const fn = (acc: number, x: number) => acc + x;
+
+  expect(a.reduce(fn, 0)).toBe(
+    a.reduce((acc: List<number>, x: number) => acc.concat(List.of(x)), List.empty()).reduce(fn, 0),
+  );
+});
+
+const fn = (x: number) => List.of(f(x));
+const gn = (x: number) => List.of(g(x));
+
+it('Chain', () => {
+  expect(a.chain(fn).chain(gn).equals(
+    a.chain((x) => fn(x).chain(gn)),
+  )).toBe(true);
+});
+
+it('Monad', () => {
+  expect(
+    List.of(42).chain(fn).equals(fn(42)),
+  ).toBe(true);
+  expect(
+    a.chain(List.of).equals(a),
+  ).toBe(true);
+});
+
+const pa = new Pending<number>((cb) => cb(42));
+
+it('Promise Setoid', (done) => {
+  pa.equals(pa).run((value) => {
+    expect(value).toBe(true);
+    done();
+  });
+});
+
+it('Promise Functor Identity', (done) => {
+  pa.map(id).equals(pa).run((value) => {
+    expect(value).toBe(true);
+    done();
+  });
+});
+
+it('Promise Functor Composition', (done) => {
+  pa.map((x) => f(g(x))).equals(pa.map(g).map(f)).run((value) => {
+    expect(value).toBe(true);
+    done();
+  });
+});
+
+const PF = new Pending<(value: number) => number>((cb) => cb(f));
+const PG = new Pending<(value: number) => number>((cb) => cb(g));
+
+it('Promise Apply', (done) => {
+  pa.ap(PF).ap(PG).equals(
+    pa.ap(PF.ap(PG.map((gn1) => (fn1) => (n) => gn1(fn1(n))))),
+  ).run((value) => {
+    expect(value).toBe(true);
+    done();
+  });
+});
+
+it('Promise Applicative Identity', (done) => {
+  pa.ap(Pending.of(id)).equals(pa).run((value) => {
+    expect(value).toBe(true);
+    done();
+  });
+});
+
+it('Promise Applicative Homomorphism', (done) => {
+  Pending.of(42).ap(Pending.of(f)).equals(Pending.of(f(42))).run((value) => {
+    expect(value).toBe(true);
+    done();
+  });
+});
+
+it('Promise Applicative Interchange', (done) => {
+  Pending.of(42).ap(PF).equals(PF.ap(Pending.of((fn1) => fn1(42)))).run((value) => {
+    expect(value).toBe(true);
+    done();
+  });
+});
+
+const pfn = (x: number) => Pending.of(f(x));
+const pgn = (x: number) => Pending.of(g(x));
+
+it('Promise Chain', (done) => {
+  pa.chain(pfn).chain(pgn).equals(
+    pa.chain((x) => pfn(x).chain(pgn)),
+  ).run((value) => {
+    expect(value).toBe(true);
+    done();
+  });
+});
+
+it('Promise Monad Left Identity', (done) => {
+  Pending.of(42).chain(pfn).equals(pfn(42)).run((value) => {
+    expect(value).toBe(true);
+    done();
+  });
+});
+
+it('Promise Monad Right Identity', (done) => {
+  pa.chain(Pending.of).equals(pa).run((value) => {
+    expect(value).toBe(true);
+    done();
+  });
+});
+
+// const sleep = (value: number, cost = 1000) => new Pending<number>((cb) => setTimeout(() => cb(value), cost));
--- src/monad/index.ts
+++ src/monad/index.ts
@@ -1,17 +1,102 @@
-export class Box<T> {
-  value: T;
+export class List<T> {
+  value: T[];
 
-  private constructor(value: T) {
+  constructor(value: T[]) {
     this.value = value;
   }
 
-  static of<P>(value: P): Box<P> {
-    return new Box(value);
+  equals(other: List<T>): boolean {
+    for (let i = 0; i < this.value.length; i++) {
+      if (this.value[i] !== other.value[i]) {
+        return false;
+      }
+    }
+    return true;
   }
 
-  map<R>(f: (value: T) => R): Box<R> {
-    return Box.of(f(this.value));
+  concat(other: List<T>): List<T> {
+    return new List(this.value.concat(other.value));
+  }
+
+  static of<P>(value: P): List<P> {
+    return new List([value]);
+  }
+
+  static empty<P = any>(): List<P> {
+    return new List([]);
+  }
+
+  map<R>(f: (value: T) => R): List<R> {
+    return new List(this.value.map(f));
+  }
+
+  ap<R>(functor: List<(value: T) => R>) {
+    return this.value.reduce((acc, value) => {
+      return functor.value.reduce((r, f) => {
+        return r.concat(List.of(f(value)));
+      }, acc);
+    }, List.empty<R>());
+  }
+
+  reduce<R>(f: (acc: R, value: T) => R, init: R) {
+    return this.value.reduce(f, init);
+  }
+
+  chain<R>(f: (value: T) => List<R>): List<R> {
+    return this.value.reduce((acc, value) => {
+      return acc.concat(f(value));
+    }, List.empty<R>());
   }
 }
 
-export const map = <T, R>(f: (x: T) => R) => (functor: Box<T>) => functor.map(f);
+export class Pending<T> {
+  private task: (cb: (value: T) => void) => void;
+
+  constructor(task: Pending<T>['task']) {
+    this.task = task;
+  }
+
+  map<R>(f: (value: T) => R) {
+    return new Pending<R>((cb) => {
+      this.task((value) => {
+        cb(f(value));
+      });
+    });
+  }
+
+  ap<R>(functor: Pending<(value: T) => R>) {
+    return new Pending<R>((cb) => {
+      this.task((value) => {
+        functor.task((fnValue) => {
+          cb(fnValue(value));
+        });
+      });
+    });
+  }
+
+  chain<R>(f: (value: T) => Pending<R>) {
+    return new Pending<R>((cb) => {
+      this.task((value) => {
+        f(value).task(cb);
+      });
+    });
+  }
+
+  static of<R>(value: R) {
+    return new Pending<R>((cb) => cb(value));
+  }
+
+  equals(other: Pending<T>) {
+    return new Pending<boolean>((cb) => {
+      this.task((lhs) => other.task((rhs) => cb(lhs === rhs)));
+    });
+  }
+
+  run(cb: (value: T) => void) {
+    return this.task(cb);
+  }
+
+  then<R>(done: (value: T) => R): R extends Pending<unknown> ? R : Pending<R> {
+    return this.map(done).chain((value) => (value instanceof Pending ? value : Pending.of(value))) as any;
+  }
+}
